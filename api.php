38706f45b9f48ed441357943d6353e93a1f90170
LeoTeiSil <161627429+LeoTeiSil@users.noreply.github.com>
2026-04-07 16:12:38 -0300

    Add files via upload

--- api.php
+++ api.php
@@ -2,12 +2,27 @@
     //CABEÇALHO
     header("Content-Type: application/json"); // Define o tipo de resposta
 
+    $metodo = $_SERVER['REQUEST_METHOD'];
+    //echo "Método da requisição: ".$metodo;
+
+    switch ($metodo) {
+        case 'GET':
+            echo "AQUI AÇÕES DO MÉTODO GET";
+            break;
+        case 'POST':
+            echo "AQUI AÇÕES DO MÉTODO POST";
+            break;
+        default:
+            echo "MÉTODO NÃO ENCONTRADO!";
+            break;
+    }
+
     //CONTEÚDO
-    $usuarios = [
-        ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
-        ["id" => 2, "nome" => "João Silva", "email" => "joao@email.com"]
-    ];
-      
+    // Susuarios = [
+    //["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
+    //["id" => 2, "nome" => "João Silva", "email" => "joao@email.com"]
+    // 1;
+
     // Converte para JSON e retorna
-    echo json_encode($usuarios);
+    //echo json_encode($usuarios);
 ?>
